5bb729f9635c0cb3062b9129e07b1f15ff38ec22
Hikigaiya <69028903+Hikigaiya@users.noreply.github.com>
2020-12-08 14:44:20 -0300

    Main Menu

---
+++ Estágio Teste/Assets/MainMenu/ScriptsMenu/MainMenuController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mainMenuPanel;
+
+    [SerializeField]
+    private GameObject mainMenuOptionsPanel;
+
+    [SerializeField]
+    private string beginGameScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainMenuPanel.SetActive(true);
+        mainMenuOptionsPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(beginGameScene);
+        //mainMenuPanel.SetActive(false);
+        //mainMenuOptionsPanel.SetActive(false);
+    }
+
+    public void OptionsMenu()
+    {
+        mainMenuPanel.SetActive(false);
+        mainMenuOptionsPanel.SetActive(true);
+    }
+
+    public void BackButton()
+    {
+        mainMenuPanel.SetActive(true);
+        mainMenuOptionsPanel.SetActive(false);
+    }
+}
---
+++ Estágio Teste/Assets/MainMenu/ScriptsMenu/SetVolume.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolume : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVol", 1f);
+    }
+    public void SetMusicLevel(float sliderValue)
+    {
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+    public void SetSFXLevel(float sliderValue)
+    {
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("SFXVol", sliderValue);
+    }
+}
